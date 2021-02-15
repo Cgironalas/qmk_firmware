30cd6403ac108ffa43d5378c8ff2f5bc127ea613
Carlos Giron <cgironalas@gmail.com>
2021-02-14 21:05:35 -0600

    Forgotten eeprom config (CGA PM Alice)

--- keyboards/handwired/cga_pm_alice_proto1/keymaps/split/config.h
+++ keyboards/handwired/cga_pm_alice_proto1/keymaps/split/config.h
@@ -19,6 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
