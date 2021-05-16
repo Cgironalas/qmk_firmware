3e99867be2995d354157fa8bbf03b63811b6e473
Carlos Giron <cgironalas@gmail.com>
2021-05-16 04:59:22 -0600

    Update vInspiration

--- keyboards/handwired/cgironalas/vInspiration/keymaps/left/keymap.c
+++ keyboards/handwired/cgironalas/vInspiration/keymaps/left/keymap.c
@@ -26,34 +26,34 @@ enum layer_names {
 // Layers
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* DVORAK */
-  [_DVORAK] = LAYOUT(
-    KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,             KC_5,  KC_6,  KC_NO,
-    KC_TAB,           KC_QUOT, KC_COMM, KC_DOT,  KC_P,             KC_Y,  KC_NO, KC_NO,
-    KC_RCTL,          KC_A,    KC_O,    KC_E,    KC_U,             KC_I,  KC_NO, KC_NO,
-    KC_LSFT,          KC_SCLN, KC_Q,    KC_J,    KC_K,             KC_X,  KC_NO, KC_NO,
-    LT(_FN1, KC_ESC), KC_LGUI, KC_LALT, KC_SPC,  LT(_FN2, KC_DEL), KC_NO, KC_NO, KC_NO
+  [_DVORAK] = LAYOUT_left(
+    KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,              KC_5,  KC_6,  KC_NO,
+    KC_TAB,           KC_QUOT, KC_COMM, KC_DOT,  KC_P,              KC_Y,  KC_NO, KC_NO,
+    KC_RCTL,          KC_A,    KC_O,    KC_E,    KC_U,              KC_I,  KC_NO, KC_NO,
+    KC_LSFT,          KC_SCLN, KC_Q,    KC_J,    KC_K,              KC_X,  KC_NO, KC_NO,
+    LT(_FN1, KC_ESC), KC_LGUI, KC_LALT, KC_SPC,  LT(_FN2, KC_BSPC), KC_NO, KC_NO, KC_NO
   ),
 
   /* QWERTY */
-  [_QWERTY] = LAYOUT(
-    KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,             KC_5,  KC_6,  KC_NO,
-    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,             KC_T,  KC_NO, KC_NO,
-    KC_RCTL,          KC_A,    KC_S,    KC_D,    KC_F,             KC_G,  KC_NO, KC_NO,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,             KC_B,  KC_NO, KC_NO,
-    LT(_FN1, KC_ESC), KC_LGUI, KC_LALT, KC_SPC,  LT(_FN2, KC_DEL), KC_NO, KC_NO, KC_NO
+  [_QWERTY] = LAYOUT_left(
+    KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,              KC_5,  KC_6,  KC_NO,
+    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,              KC_T,  KC_NO, KC_NO,
+    KC_RCTL,          KC_A,    KC_S,    KC_D,    KC_F,              KC_G,  KC_NO, KC_NO,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,              KC_B,  KC_NO, KC_NO,
+    LT(_FN1, KC_ESC), KC_LGUI, KC_LALT, KC_SPC,  LT(_FN2, KC_BSPC), KC_NO, KC_NO, KC_NO
   ),
 
   /* FN1 Function layer triggered from the left hand. */
-  [_FN1] = LAYOUT(
-    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,            KC_F5, KC_F6, KC_NO,
-    KC_TAB,           KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,            KC_NO, KC_NO, KC_NO,
-    KC_LCTL,          KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,            KC_NO, KC_NO, KC_NO,
-    KC_LSFT,          KC_NO,   KC_NO,   KC_NO,   KC_NO,            DF(1), KC_NO, KC_NO,
-    KC_TRNS,          KC_LGUI, KC_LALT, KC_SPC,  KC_BSPC,          KC_NO, KC_NO, KC_NO
+  [_FN1] = LAYOUT_left(
+    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,             KC_F5, KC_F6, KC_NO,
+    KC_TAB,           KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,             KC_NO, KC_NO, KC_NO,
+    KC_LCTL,          KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,             KC_NO, KC_NO, KC_NO,
+    KC_LSFT,          KC_NO,   KC_NO,   KC_NO,   KC_NO,             DF(1), KC_NO, KC_NO,
+    KC_TRNS,          KC_LGUI, KC_LALT, KC_SPC,  KC_BSPC,           KC_NO, KC_NO, KC_NO
   ),
 
   /* FN2 Function layer triggered from the right hand. */
-  [_FN2] = LAYOUT(
+  [_FN2] = LAYOUT_left(
     KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,            KC_F5, KC_F6, KC_NO,
     KC_TAB,           KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU,          KC_NO, KC_NO, KC_NO,
     KC_LCTL,          KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT,          KC_NO, KC_NO, KC_NO,
--- keyboards/handwired/cgironalas/vInspiration/vInspiration.h
+++ keyboards/handwired/cgironalas/vInspiration/vInspiration.h
@@ -53,7 +53,7 @@
  * k80: KC_SPC,   k81: KC_NO,   k82: KC_NO,   k83: KC_NO,   k84: KC_NO,   k85: KC_RALT, k86: KC_NO,   k87: KC_LCTL
  */
 
-#define LAYOUT( \
+#define LAYOUT_right( \
   k00, k01, k02, k03, k04, k05, k06, k07, \
   k10, k11, k12, k13, k14, k15, k16, k17, \
   k20, k21, k22, k23, k24, k25, k26, k27, \
@@ -61,7 +61,22 @@
   k40, k41, k42, k43, k44, k45, k46, k47  \
 ) \
 { \
-  { k00,   k01,   k02,   k03,   k04,   k05,   k06,   KC_NO   }, \
+  { KC_NO, k01,   k02,   k03,   k04,   k05,   k06,   k07   }, \
+  { k10,   k11,   k12,   k13,   k14,   k15,   k16,   k17   }, \
+  { k20,   k21,   k22,   k23,   k24,   k25,   k26,   KC_NO }, \
+  { k30,   k31,   k32,   k33,   k34,   k35,   KC_NO, KC_NO }, \
+  { k40,   k41,   k42,   k43,   k44,   k45,   KC_NO, KC_NO }  \
+}
+
+#define LAYOUT_left( \
+  k00, k01, k02, k03, k04, k05, k06, k07, \
+  k10, k11, k12, k13, k14, k15, k16, k17, \
+  k20, k21, k22, k23, k24, k25, k26, k27, \
+  k30, k31, k32, k33, k34, k35, k36, k37, \
+  k40, k41, k42, k43, k44, k45, k46, k47  \
+) \
+{ \
+  { k00,   k01,   k02,   k03,   k04,   k05,   k06,   KC_NO }, \
   { k10,   k11,   k12,   k13,   k14,   k15,   KC_NO, KC_NO }, \
   { k20,   k21,   k22,   k23,   k24,   k25,   KC_NO, KC_NO }, \
   { k30,   k31,   k32,   k33,   k34,   k35,   KC_NO, KC_NO }, \
